e6e5afaf4ecc2e74d2a5e3da3512f943a49d8b9a
Sean Collings <nightharrier@gmail.com>
2022-08-22 20:46:29 +0200

    feature: add initial login page

---
+++ pages/login.tsx
@@ -0,0 +1,13 @@
+import Header from '@components/header';
+import { NextPage } from 'next';
+
+const LoginPage: NextPage = () => {
+  return (
+    <>
+      <Header />
+      <div>Login Page</div>
+    </>
+  );
+};
+
+export default LoginPage;
