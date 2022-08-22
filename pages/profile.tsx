8a6316171478f095c5620e0438c6ad75a0cd0944
Sean Collings <nightharrier@gmail.com>
2022-08-22 20:46:20 +0200

    feature: add initial profile page

---
+++ pages/profile.tsx
@@ -0,0 +1,13 @@
+import Header from '@components/header';
+import { NextPage } from 'next';
+
+const ProilePage: NextPage = () => {
+  return (
+    <>
+      <Header />
+      <div>Profile Page</div>
+    </>
+  );
+};
+
+export default ProilePage;
