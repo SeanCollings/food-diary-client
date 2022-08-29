3884cc8687a8552f077dc32b59c59d41598e7d46
Sean Collings <nightharrier@gmail.com>
2022-08-29 22:16:51 +0200

    add new radio button component

---
+++ components/ui/radio-button/index.tsx
@@ -0,0 +1,69 @@
+import { COLOURS } from '@utils/constants';
+import { ChangeEvent, FC } from 'react';
+import styled from 'styled-components';
+
+interface ISContainer {
+  colour?: string;
+}
+
+const SContainer = styled.div<ISContainer>`
+  display: flex;
+  align-items: center;
+
+  input[type='radio'] {
+    transition: 0.1s;
+    appearance: none;
+    min-width: 16px;
+    height: 16px;
+    border-radius: 16px;
+    outline: none;
+    margin-right: 9.5px;
+    cursor: pointer;
+    background-color: ${COLOURS.white_off};
+    border: 2px solid ${COLOURS.gray_dark};
+  }
+  input[type='radio']:checked {
+    border: 5px solid ${({ colour }) => colour || COLOURS.black};
+  }
+`;
+const SRadio = styled.input`
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.6;
+  }
+`;
+
+interface IRadioButtonProps {
+  id: string | number;
+  value: string | number;
+  name: string;
+  colour?: string;
+  onChange: (value: string) => void;
+}
+
+const RadioButton: FC<IRadioButtonProps> = ({
+  id,
+  value,
+  name,
+  colour,
+  onChange,
+}) => {
+  const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    onChange(event.target.value);
+  };
+
+  return (
+    <SContainer colour={colour}>
+      <SRadio
+        id={`${id}-radio`}
+        type={'radio'}
+        value={value}
+        name={name}
+        onChange={onChangeHandler}
+      />
+    </SContainer>
+  );
+};
+
+export default RadioButton;
