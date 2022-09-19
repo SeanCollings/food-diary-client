de1b783a4c585d45c0396930af1834098ab25958
Sean Collings <nightharrier@gmail.com>
2022-09-19 09:38:14 +0200

    refactor time-input-custom for better experience

--- components/ui/input/time-input-custom/index.tsx
+++ components/ui/input/time-input-custom/index.tsx
@@ -1,9 +1,14 @@
 import { COLOURS, OPACITY_40 } from '@utils/constants';
+import {
+  formatFinalTime,
+  getFormatMinutesWithHours,
+  getSplitTime,
+} from '@utils/time-utils';
 import { ChangeEvent, FC, useEffect, useState } from 'react';
 import styled from 'styled-components';
 
-const MAX_HOURS = 23;
-const MAX_MINUTES = 59;
+const MAX_INPUT_HOURS = 23;
+const MAX_INPUT_NUMBER = 99;
 
 interface ISInput {
   isEmpty: boolean;
@@ -30,9 +35,9 @@ const SInput = styled.input<ISInput>`
   text-align: center;
   outline: none;
   border-radius: 4px;
-  padding: 2px 4px;
+  padding: 1px 4px;
   width: 40px;
-  font-size: 17px;
+  font-size: 18px;
   box-shadow: inset 0 3px ${COLOURS.gray_light};
   border: 1px dashed ${COLOURS.gray_dark}${OPACITY_40};
 
@@ -52,34 +57,17 @@ const SLabel = styled.label`
   color: ${COLOURS.gray_dark};
 `;
 
-type TTime = 'hours' | 'minutes';
+type TTimeType = 'hours' | 'minutes';
 
 interface IComponentProps {
   id: string;
   value: string;
   onChange: (value: string) => void;
-  onBlur: () => void;
+  onBlur?: () => void;
 }
 
-const formatFinalTime = (hours: string, minutes: string) => {
-  let formattedHours = hours;
-  let formattedMinutes = minutes;
-
-  if (!formattedHours || +formattedHours > MAX_HOURS) {
-    formattedHours = '00';
-  }
-  if (!formattedMinutes || +formattedMinutes > MAX_MINUTES) {
-    formattedMinutes = '00';
-  }
-
-  return `${hours}:${minutes}`;
-};
-
-const getTime = (value: string) => {
-  const [hours, minutes] = value.split(':');
-
-  return [hours ?? '00', minutes ?? '00'];
-};
+const getHoursId = (id: string) => `${id}_hours`;
+const getMinutesId = (id: string) => `${id}_minutes`;
 
 const TimeInputCustom: FC<IComponentProps> = ({
   id,
@@ -91,76 +79,85 @@ const TimeInputCustom: FC<IComponentProps> = ({
   const [minutes, setMinutes] = useState('00');
 
   useEffect(() => {
-    const [hours, minutes] = getTime(value);
+    const [hours, minutes] = getSplitTime(value);
     setHours(hours);
     setMinutes(minutes);
   }, [value]);
 
   const handleOnChange =
-    (time: TTime) => (event: ChangeEvent<HTMLInputElement>) => {
+    (type: TTimeType) => (event: ChangeEvent<HTMLInputElement>) => {
       const { value } = event.target;
       let newValue = value;
 
-      if (newValue.length > 2) {
+      if (newValue.length > 2 && +newValue !== MAX_INPUT_NUMBER + 1) {
         newValue = value.slice(0, 2);
       }
 
-      if (time === 'hours') {
-        if (+newValue > MAX_HOURS || !newValue) {
-          newValue = '00';
-        } else if (+newValue < 0) {
-          newValue = `${MAX_HOURS}`;
+      if (type === 'hours') {
+        if (!newValue || +newValue === MAX_INPUT_HOURS + 1) {
+          newValue = '0';
+        } else if (+newValue === -1) {
+          newValue = MAX_INPUT_HOURS.toString();
         }
 
-        setHours(newValue);
-      } else {
-        if (+newValue > MAX_MINUTES || !newValue) {
-          newValue = '00';
-        } else if (+newValue < 0) {
-          newValue = `${MAX_MINUTES}`;
-        }
-        setMinutes(newValue);
+        return setHours(newValue);
       }
+
+      if (!newValue || +newValue === MAX_INPUT_NUMBER + 1) {
+        newValue = '0';
+      } else if (+newValue === -1) {
+        newValue = MAX_INPUT_NUMBER.toString();
+      }
+
+      setMinutes(newValue);
     };
   const handleOnBlur = () => {
-    let updatedHours = hours;
-    let updatedMinutes = minutes;
+    const [updatedHours, updatedMinutes] = getFormatMinutesWithHours(
+      hours,
+      minutes
+    );
 
-    if (hours.length === 1) {
-      updatedHours = `0${hours}`;
-      setHours(updatedHours);
-    }
-    if (minutes.length === 1) {
-      updatedMinutes = `0${minutes}`;
-      setMinutes(updatedMinutes);
-    }
-
+    setHours(updatedHours);
+    setMinutes(updatedMinutes);
     onChange(formatFinalTime(updatedHours, updatedMinutes));
-    onBlur();
+    onBlur?.();
+  };
+  const handleOnFocus = (id: string) => () => {
+    const element = document.getElementById(id) as HTMLInputElement;
+
+    if (!element) {
+      return;
+    }
+
+    element.select();
   };
 
+  const isEmpty = hours === '00' && minutes === '00';
+
   return (
-    <SContainer title="HH:MM">
+    <SContainer title="hh:mm">
       <SInput
-        id={`${id}-hours`}
-        onChange={handleOnChange('hours')}
-        onBlur={handleOnBlur}
+        id={getHoursId(id)}
         type="number"
         value={hours}
         maxLength={2}
-        isEmpty={hours === '00' && minutes === '00'}
+        isEmpty={isEmpty}
+        onChange={handleOnChange('hours')}
+        onFocus={handleOnFocus(getHoursId(id))}
+        onBlur={handleOnBlur}
       />
       <span>:</span>
       <SInput
-        id={`${id}-minutes`}
-        onChange={handleOnChange('minutes')}
-        onBlur={handleOnBlur}
+        id={getMinutesId(id)}
         type="number"
         value={minutes}
         maxLength={2}
-        isEmpty={hours === '00' && minutes === '00'}
+        isEmpty={isEmpty}
+        onChange={handleOnChange('minutes')}
+        onFocus={handleOnFocus(getMinutesId(id))}
+        onBlur={handleOnBlur}
       />
-      <SLabel htmlFor={`${id}-hours`}>time (hh : mm)</SLabel>
+      <SLabel htmlFor={getHoursId(id)}>time (hh : mm)</SLabel>
     </SContainer>
   );
 };
