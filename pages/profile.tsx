7994225758be20fd1ade3e429c84003ba9f91ea4
Sean Collings <nightharrier@gmail.com>
2022-09-22 15:42:00 +0200

    minor initial profile updates

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,4 +1,4 @@
-import { APP_THEME_DEFAULT, COLOURS } from '@utils/constants';
+import { APP_THEME_DEFAULT, COLOURS, OPACITY_40 } from '@utils/constants';
 import { NextPage } from 'next';
 import { MdAccountCircle, MdPhotoCamera } from 'react-icons/md';
 import styled from 'styled-components';
@@ -9,11 +9,22 @@ const SProfileContainer = styled.section`
   flex-wrap: wrap;
 `;
 const SMyProfile = styled.div`
+  position: relative;
   background: ${COLOURS.white};
   flex: 1;
   border-radius: 20px;
   padding: 40px;
-  min-width: 400px;
+  min-width: 380px;
+  max-width: 400px;
+`;
+const STopContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding-bottom: 16px;
+  border-bottom: 1px solid ${COLOURS.gray}${OPACITY_40};
+  margin-bottom: 16px;
 `;
 const SAvatarContainer = styled.div`
   display: flex;
@@ -41,6 +52,38 @@ const SProfileLife = styled.div`
   padding: 40px;
   min-width: 700px;
 `;
+const SEmail = styled.div`
+  line-height: 1.02em;
+  color: ${COLOURS.gray};
+  font-size: 15px;
+`;
+const SProfileHeader = styled.div`
+  line-height: 1.02em;
+  font-size: 24px;
+  font-weight: bold;
+  padding-bottom: 8px;
+`;
+const SName = styled.div`
+  font-size: 16px;
+  padding-bottom: 8px;
+`;
+const SButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px;
+`;
+const SShareButton = styled.button`
+  outline: none;
+  cursor: pointer;
+  font-size: 22px;
+  padding: 8px 12px;
+  border: 1px solid;
+  border-radius: 8px;
+  height: 50px;
+  background: ${APP_THEME_DEFAULT.primary};
+  color: ${APP_THEME_DEFAULT.textLight};
+`;
 
 enum EProfileLife {
   MY_STATS = 'my_stats',
@@ -51,20 +94,24 @@ const ProilePage: NextPage = () => {
   return (
     <SProfileContainer>
       <SMyProfile>
-        <SAvatarContainer>
-          <SAvatar
-            size={100}
-            color={APP_THEME_DEFAULT.primary}
-            title="Change avatar"
-          />
-          <SCameraContainer>
-            <MdPhotoCamera size={24} />
-          </SCameraContainer>
-        </SAvatarContainer>
-        <div>My Profile</div>
-        <div>Name Surname</div>
-        <div>email@address.com</div>
-        <div>Share Profile</div>
+        <STopContainer>
+          <SAvatarContainer>
+            <SAvatar
+              size={100}
+              color={APP_THEME_DEFAULT.primary}
+              title="Change avatar"
+            />
+            <SCameraContainer>
+              <MdPhotoCamera size={24} />
+            </SCameraContainer>
+          </SAvatarContainer>
+          <SEmail>email@address.com</SEmail>
+        </STopContainer>
+        <SProfileHeader>My Profile</SProfileHeader>
+        <SName>Name Surname</SName>
+        <SButtonContainer>
+          <SShareButton>Share Profile</SShareButton>
+        </SButtonContainer>
       </SMyProfile>
       <SProfileLife>1</SProfileLife>
     </SProfileContainer>
