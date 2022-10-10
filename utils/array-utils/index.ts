9106a21d5e99293eb761999dfb1501f7dee6efe8
Sean Collings <nightharrier@gmail.com>
2022-10-10 17:54:39 +0200

    add getrange nad get-padded-nested-array utils

---
+++ utils/array-utils/index.ts
@@ -0,0 +1,59 @@
+/**
+ * Creates an array of a length populated from `start` to `end`
+ * @param start number
+ * @param end number
+ * @returns number[]
+ */
+export const getRange = (start: number, end: number) => {
+  const length = end - start + 1;
+
+  if (start > end || length < 0) {
+    return [];
+  }
+
+  return Array.from({ length }, (_, index) => index + start);
+};
+
+/**
+ * Converts an array of strings into an array of string arrays depending on
+ * number of maxItems and maxItemsPerRow
+ * i.e. ['1',2','3','4'] can become [['1','2'],['3','4']]
+ * @param values string[]
+ * @param maxItems number
+ * @param maxItemsPerRow number
+ * @returns string[][]
+ */
+export const getPaddedNestedArray = (
+  values: string[],
+  maxItems: number,
+  maxItemsPerRow: number
+) => {
+  const nestedArray: string[][] = [];
+  let temp: string[] = [];
+  let lastFullSet = 0;
+
+  for (let i = 1; i < maxItems + 1; i++) {
+    const lessThanMaxElements = maxItems < maxItemsPerRow;
+    const fullRowElements = i % maxItemsPerRow === 0;
+    const lastElement = maxItems > maxItemsPerRow && i === maxItems;
+
+    if (lessThanMaxElements && i === 1) {
+      nestedArray.push([
+        ...values.slice(0, maxItems),
+        ...Array(maxItemsPerRow - maxItems).fill(''),
+      ]);
+    } else if (fullRowElements) {
+      temp = values.slice(i - maxItemsPerRow, i);
+      nestedArray.push(temp);
+      temp = [];
+      lastFullSet = i;
+    } else if (lastElement) {
+      nestedArray.push([
+        ...values.slice(lastFullSet, maxItems),
+        ...Array(maxItemsPerRow - (maxItems - lastFullSet)).fill(''),
+      ]);
+    }
+  }
+
+  return nestedArray;
+};
