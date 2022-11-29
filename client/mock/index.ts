074e3684c8d8e6914c8988161df0540033b997c7
Sean Collings <nightharrier@gmail.com>
2022-11-29 18:59:19 +0200

    remove trends mock data

--- client/mock/index.ts
+++ client/mock/index.ts
@@ -1,6 +1,5 @@
 export * from '@client/mock/user.mock';
 export * from '@client/mock/trends-excercise.mock';
 export * from '@client/mock/trends-wellness.mock';
-export * from '@client/mock/trends-meals.mock';
 export * from '@client/mock/diary-meals.mock';
 export * from '@client/mock/diary-wellness.mock';
