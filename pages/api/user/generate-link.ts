e65b43169e271293f684e34bd27727e8574de3bb
Sean Collings <nightharrier@gmail.com>
2022-11-29 19:01:54 +0200

    update generate-link api

--- pages/api/user/generate-link.ts
+++ pages/api/user/generate-link.ts
@@ -1,29 +1,49 @@
-import { createGuid } from '@utils/string-utils';
+import axios, { AxiosError } from 'axios';
 import { NextApiRequest, NextApiResponse } from 'next';
+import { getSession } from 'next-auth/react';
 
 interface IResponse {
-  shareLink: string;
+  shareLink?: string;
+  ok?: boolean;
+  message?: string;
 }
-
-const generateShareLink = async () => {
-  return {
-    shareLink: createGuid(),
-  };
-};
 
 const handler = async (
   req: NextApiRequest,
   res: NextApiResponse<IResponse>
 ) => {
+  const session = await getSession({ req });
+
   if (req.method === 'POST') {
-    console.log('GENERATE LINK POST:', req.body);
+    console.log('GENERATE LINK POST:');
 
-    const { shareLink } = await generateShareLink();
+    try {
+      const { data } = await axios.put(
+        `${process.env.SERVER_HOST}/share/generate-link`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${session?.accessToken}`,
+          },
+        }
+      );
 
-    return res.status(201).json({ shareLink });
+      return res.status(201).json({ ok: true, shareLink: data.shareLink });
+    } catch (err) {
+      return res.status(500).json({
+        ok: false,
+        message: (
+          err as AxiosError<{
+            statusCode: number;
+            message: string;
+            error: string;
+          }>
+        ).response?.data.message,
+      });
+    }
   }
 
-  return;
+  return res.status(500).json({ ok: false });
 };
 
 export default handler;
