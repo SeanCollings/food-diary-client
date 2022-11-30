91328edbf16098fce9bcf5fa1090e7f2132b02f8
Sean Collings <nightharrier@gmail.com>
2022-11-30 19:35:55 +0200

    update api-user-profile to request server

--- pages/api/user/index.ts
+++ pages/api/user/index.ts
@@ -1,3 +1,4 @@
+import { IUserResponse } from '@client/interfaces/user-data.type';
 import axios, { AxiosError } from 'axios';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getSession } from 'next-auth/react';
@@ -6,14 +7,34 @@ interface IResponse {
   ok: boolean;
   message?: string;
 }
+interface IGetUserProfileResponse {
+  ok: boolean;
+  message?: string;
+  user: IUserResponse;
+}
 
 const handler = async (
   req: NextApiRequest,
-  res: NextApiResponse<IResponse>
+  res: NextApiResponse<IResponse | IGetUserProfileResponse>
 ) => {
   const session = await getSession({ req });
 
   try {
+    if (req.method === 'GET') {
+      console.log('-------- /user/profile');
+
+      const { data } = await axios.get<IUserResponse>(
+        `${process.env.SERVER_HOST}/user/profile`,
+        {
+          headers: {
+            Authorization: `Bearer ${session?.accessToken}`,
+          },
+        }
+      );
+
+      return res.status(201).json({ ok: true, user: data });
+    }
+
     if (req.method === 'PATCH') {
       const { data } = await axios.patch(
         `${process.env.SERVER_HOST}/user`,
