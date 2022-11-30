f7a5e1e628ce99178cc4fb239334a3000c7481ed
Sean Collings <nightharrier@gmail.com>
2022-11-30 19:34:13 +0200

    update api-diary to request server

--- pages/api/diary/index.ts
+++ pages/api/diary/index.ts
@@ -1,16 +1,8 @@
-import { diaryMealsMockData, diaryWellnessMockData } from '@client/mock';
-import {
-  TWellnessEntry,
-  TWellnessValueTypes,
-} from '@lib/interfaces/wellness.interface';
-import { TMealCard } from '@utils/interfaces';
 import { NextApiRequest, NextApiResponse } from 'next';
-import {
-  getMidnightISODaysInMonth,
-  getMonthAndYearFromDate,
-} from '@utils/date-utils';
 import { URI_DIARY } from '@client/constants';
 import { IDiaryReponseData } from '@client/interfaces/diary-data';
+import axios, { AxiosError } from 'axios';
+import { getSession } from 'next-auth/react';
 
 interface IRequest extends NextApiRequest {
   query: {
@@ -18,65 +10,47 @@ interface IRequest extends NextApiRequest {
   };
 }
 
-export const getMealEntriesForMonth = (date: string) => {
-  const [month, year] = getMonthAndYearFromDate(date);
-  const datesInMonth = getMidnightISODaysInMonth(month, year);
-
-  const mealsEntries = datesInMonth.reduce((acc, date) => {
-    if (diaryMealsMockData[date]) {
-      acc[date] = diaryMealsMockData[date];
-    }
-
-    return acc;
-  }, {} as { [date: string]: TMealCard });
-
-  return mealsEntries;
-};
-export const getWellessEntriesForMonth = (date: string) => {
-  const [month, year] = getMonthAndYearFromDate(date);
-  const datesInMonth = getMidnightISODaysInMonth(month, year);
-
-  const wellnessEntries = datesInMonth.reduce((acc, date) => {
-    if (diaryWellnessMockData[date]) {
-      acc[date] = diaryWellnessMockData[date];
-    }
-
-    return acc;
-  }, {} as { [date: string]: TWellnessEntry<TWellnessValueTypes> });
-
-  return wellnessEntries;
-};
-
-const getDataFromAPI = async (date: string) => {
-  const meals = getMealEntriesForMonth(date);
-  const wellness = getWellessEntriesForMonth(date);
-
-  return Promise.resolve({
-    meals,
-    wellness,
-  });
-};
-
 const handler = async (
   req: IRequest,
   res: NextApiResponse<IDiaryReponseData>
 ) => {
+  const session = await getSession({ req });
   const { date } = req.query;
   console.log(`-------- ${URI_DIARY} :`, date);
 
   if (req.method !== 'GET') {
-    return;
+    return res.status(500).json({ ok: false });
   }
 
-  const { meals, wellness } = await getDataFromAPI(date);
+  try {
+    const { data } = await axios.get(
+      `${process.env.SERVER_HOST}/diary?date=${date}`,
+      {
+        headers: {
+          Authorization: `Bearer ${session?.accessToken}`,
+        },
+      }
+    );
 
-  return res.status(200).json({
-    entry: {
-      date,
-      meals,
-      wellness,
-    },
-  });
+    return res.status(200).json({
+      ok: true,
+      entry: {
+        meals: data?.meals || {},
+        wellness: data?.wellness || {},
+      },
+    });
+  } catch (err) {
+    return res.status(500).json({
+      ok: false,
+      message: (
+        err as AxiosError<{
+          statusCode: number;
+          message: string;
+          error: string;
+        }>
+      ).response?.data.message,
+    });
+  }
 };
 
 export default handler;
