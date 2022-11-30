dc4168cee0bf246c21f3c2c736ba0a38cf2176e2
Sean Collings <nightharrier@gmail.com>
2022-11-30 19:34:35 +0200

    update api-wellness to request server

--- pages/api/wellness/index.ts
+++ pages/api/wellness/index.ts
@@ -1,18 +1,53 @@
+import { IWellnessEntriesDto } from '@lib/interfaces/wellness.interface';
+import axios, { AxiosError } from 'axios';
 import { NextApiRequest, NextApiResponse } from 'next';
+import { getSession } from 'next-auth/react';
 
+interface IRequest<T extends unknown> extends NextApiRequest {
+  body: T;
+}
 interface IResponse {
   ok: boolean;
   message?: string;
 }
 
 const handler = async (
-  req: NextApiRequest,
+  req: IRequest<IWellnessEntriesDto>,
   res: NextApiResponse<IResponse>
 ) => {
-  if (req.method === 'POST') {
-    console.log('WELLNESS POST:', req.body);
+  const session = await getSession({ req });
 
-    return res.status(201).json({ ok: true });
+  try {
+    if (req.method === 'PUT') {
+      const transformedBody = Object.values(req.body).reduce((acc, entry) => {
+        acc.push(entry);
+
+        return acc;
+      }, [] as any);
+
+      const { data } = await axios.put(
+        `${process.env.SERVER_HOST}/wellness`,
+        { data: transformedBody },
+        {
+          headers: {
+            Authorization: `Bearer ${session?.accessToken}`,
+          },
+        }
+      );
+
+      return res.status(201).json({ ok: true });
+    }
+  } catch (err) {
+    return res.status(500).json({
+      ok: false,
+      message: (
+        err as AxiosError<{
+          statusCode: number;
+          message: string;
+          error: string;
+        }>
+      ).response?.data.message,
+    });
   }
 
   return;
