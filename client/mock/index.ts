728cd16b76adbd63e0d8a8edb724f92909886439
Sean Collings <nightharrier@gmail.com>
2022-11-30 20:27:31 +0200

    remove mock-data

--- client/mock/index.ts
+++
@@ -1,5 +0,0 @@
-export * from '@client/mock/user.mock';
-export * from '@client/mock/trends-excercise.mock';
-export * from '@client/mock/trends-wellness.mock';
-export * from '@client/mock/diary-meals.mock';
-export * from '@client/mock/diary-wellness.mock';
