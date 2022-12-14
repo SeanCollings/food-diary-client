652e5131a094cd7c7b33dc2055cf65f84518664f
Sean Collings <nightharrier@gmail.com>
2022-12-14 18:31:11 +0200

    create server api-client and endpoints

---
+++ server/api-client/index.ts
@@ -0,0 +1,33 @@
+import axios, { HeadersDefaults, RawAxiosRequestHeaders } from 'axios';
+import { NextApiRequest } from 'next';
+import { Session } from 'next-auth';
+import { getSession } from 'next-auth/react';
+
+type AxiosHeaders =
+  | Partial<HeadersDefaults>
+  | Partial<RawAxiosRequestHeaders>
+  | { Authorization?: string };
+
+const createInstance = (
+  requiresAuthorization: boolean,
+  session?: Session | null
+) => {
+  const headers: AxiosHeaders = { Authorization: '' };
+
+  if (requiresAuthorization) {
+    headers['Authorization'] = `Bearer ${session?.accessToken}`;
+  }
+
+  return axios.create({
+    baseURL: process.env.SERVER_HOST,
+    timeout: 2000,
+    headers,
+  });
+};
+
+export const apiClient = createInstance(false);
+
+export const createApiClientSecure = async (req: NextApiRequest) => {
+  const session = await getSession({ req });
+  return createInstance(true, session);
+};
---
+++ server/server.constants.ts
@@ -0,0 +1,19 @@
+export const API_DIARY = '/diary';
+export const API_DIARY_CALENDAR_ENTRIES = '/diary/calendar-entries';
+
+export const API_MEALS = '/meals';
+
+export const API_SHARE = '/share';
+export const API_SHARE_GENERATE_LINK = '/share/generate-link';
+export const API_SHARE_LINK_SHAREABLE = '/share/link-shareable';
+
+export const API_SUMMARY = '/summary';
+
+export const API_TRENDS_BEVERAGE = '/trends/beverage-trends';
+export const API_TRENDS_EXCERCISE = '/trends/excercise-trends';
+export const API_TRENDS_MEAL = '/trends/meal-trends';
+
+export const API_USER_PROFILE = '/user/profile';
+export const API_USER_PREFERENCES = '/user/preferences';
+
+export const API_WELLNESS = '/wellness';
