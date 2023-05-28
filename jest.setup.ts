0fbe8328b12b52803c74d28dc5695f62412b700a
Sean Collings <nightharrier@gmail.com>
2023-05-28 18:43:11 +0200

    add jest usefaketimers

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,5 +2,5 @@ import '@testing-library/jest-dom/extend-expect';
 
 global.beforeAll(async () => {
   jest.spyOn(Date, 'now').mockImplementation(() => 1682632800000); // '2022-04-27'
-  jest.setSystemTime(new Date('2022-04-27'));
+  jest.useFakeTimers().setSystemTime(new Date('2022-04-27'));
 });
